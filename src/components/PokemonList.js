c7a01786f79c83daf895570877d7d18116aa28d3
dilancirkin <dilancirkin@gmail.com>
2021-07-30 12:32:20 +0300

    pokemon image is showing on the home screen

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -1,61 +1,82 @@
-import React, { Component } from 'react';
-import {StyleSheet, View,Text,FlatList,TouchableOpacity, Image} from 'react-native';
+import React, {Component} from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  FlatList,
+  TouchableOpacity,
+  Image,
+} from 'react-native';
 import axios from 'axios';
-import styles from "./PokemonList.style.js"
+import styles from './PokemonList.style.js';
 
+class PokemonList extends Component {
+  //extend ile react componentinin tüm özelliklerini aktardık
+  constructor(props) {
+    super(props);
+    this.state = {
+      pokemonList: [],
+    };
+  }
 
-class PokemonList extends Component{//extend ile react componentinin tüm özelliklerini aktardık 
-    constructor(props){
-        super(props)
-        this.state={
-            pokemonList:[],
-        };
-    }  
-        componentDidMount(){
-             axios.get('https://pokeapi.co/api/v2/pokemon')
-              .then((data)=>{
-                  this.setState({pokemonList:data.data.results},()=>{
-                      console.log(this.state.pokemonList);
-                  });
-              })
-              .catch((error)=>{
-                  console.log(error);
-              
-              });
-         }
+  async componentDidMount() {
+    const newData = [];
 
-         renderItem= ({item})=>{
-             const{navigate}=this.props.navigation;
-             return(
-                <View
-                style={styles.cardStyle}>
-               <Text style={styles.textStyle}>{item.name}</Text>
-               <TouchableOpacity onPress={() => navigate('PokemonDetailScreen',{pokemon:item})}>
-               <View style={styles.cardBackgroundStyle}>
-              <Image source={require("../../assets/pokemon.png")} 
-                    style={styles.pokemonImageStyle}></Image>
+    await axios
+      .get('https://pokeapi.co/api/v2/pokemon')
+      .then(async data => {
+        for (let index = 0; index < data.data.results.length; index++) {
+          const element = data.data.results[index];
+          await axios.get(element.url).then(data => {
+            console.log(element.url);
+            const item = {
+              name: data.data.species.name,
+              image: data.data.sprites.front_default,
+              url: element.url,
+            };
+            newData.push(item);
+          });
+        }
+        console.log(newData);
+        this.setState({pokemonList: newData}, () => {
+          console.log('ee: ', this.state.pokemonList);
+        });
+      })
+
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  renderItem = data => {
+    const {navigate} = this.props.navigation;
+
+    console.log('ab', data);
+    return (
+      <View style={styles.cardStyle}>
+        <Text style={styles.textStyle}>{data.name}</Text>
+        <TouchableOpacity
+          onPress={() => navigate('PokemonDetailScreen', {pokemon: data})}>
+          <View style={styles.cardBackgroundStyle}>
+            <Image
+              source={{uri: data.image}}
+              style={styles.pokemonImageStyle}></Image>
           </View>
-              
+        </TouchableOpacity>
+      </View>
+    );
+  };
 
-               </TouchableOpacity>
-            
-                </View>
-             );
-             
-         };
-     
-    
-     render(){
-         return (
-             <View>
-                 <FlatList
-                 numColumns={2}
-                 data={this.state.pokemonList}
-                 renderItem={this.renderItem}
-                 />
-             </View>
-         );
-     }
-     
-    }
+  render() {
+    return (
+      <View>
+        <FlatList
+          numColumns={2}
+          data={this.state.pokemonList}
+          renderItem={({item}) => this.renderItem(item)}
+        />
+      </View>
+    );
+  }
+}
 export default PokemonList;
--- src/screens/PokemonDetailScreen.js
+++ src/screens/PokemonDetailScreen.js
@@ -1,31 +1,44 @@
-import React, { Component, useEffect, useState} from 'react';
+import React, {Component, useEffect, useState} from 'react';
 import axios from 'axios';
-import {StyleSheet,View,Text,Image} from 'react-native';
-import styles from "./PokemonDetailScreen.style.js"
+import {StyleSheet, View, Text, Image} from 'react-native';
+import styles from './PokemonDetailScreen.style.js';
 
+const PokemonDetailScreen = props => {
+  const [pokemonDetailData, setPokemonDetailData] = useState();
+  useEffect(() => {
+    const pokemon = props.route.params.pokemon;
+    axios.get(pokemon.url).then(response => {
+      const data = response.data;
+      setPokemonDetailData(data);
+    });
+  });
+  return (
+    <View>
+      <Image
+        source={{
+          uri: pokemonDetailData && pokemonDetailData.sprites.front_default,
+        }}
+        style={styles.pokeImageStyle}></Image>
+      <Text style={styles.baseText}>
+        {'Base experience\n'}
+        <Text style={styles.innerText}>
+          {pokemonDetailData && pokemonDetailData.base_experience}
+        </Text>
+      </Text>
+      <Text style={styles.baseText}>
+        {'Version Name\n'}
+        <Text style={styles.innerText}>
+          {pokemonDetailData && pokemonDetailData.game_indices[0].version.name}
+        </Text>
+      </Text>
+      <Text style={styles.baseText}>
+        {'Ability Name\n'}
+        <Text style={styles.innerText}>
+          {pokemonDetailData && pokemonDetailData.abilities[0].ability.name}
+        </Text>
+      </Text>
+    </View>
+  );
+};
 
-const PokemonDetailScreen=(props)=> {    
-    const [pokemonDetailData, setPokemonDetailData]=useState()
-useEffect(()=>{
-    const pokemon=props.route.params.pokemon;
-    axios.get(pokemon.url) .then((response)=>{
-        const data= response.data;
-        setPokemonDetailData(data)
-    })
-})
-    return(
-        <View>
-            <Image source={require("../../assets/pokemon.png")} style={styles.pokeImageStyle}></Image>
-            <Text style={styles.baseText}>{'Base experience\n'}<Text style={styles.innerText}>{pokemonDetailData && pokemonDetailData.base_experience}</Text></Text>
-            <Text style={styles.baseText}>{'Version Name\n'}<Text style={styles.innerText}>{pokemonDetailData && pokemonDetailData.game_indices[0].version.name}</Text></Text>
-            <Text style={styles.baseText}>{'Ability Name\n'}<Text style={styles.innerText}>{pokemonDetailData && pokemonDetailData.abilities[0].ability.name}</Text></Text>
-        </View>
-    );
-
-
-}
-
-
-
-    
 export default PokemonDetailScreen;
